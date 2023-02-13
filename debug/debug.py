2b901b46b8071bc2031b0864d74dab37b28a5487
hangilzzang <gkrwhddlwjqrl@gmail.com>
2023-02-13 18:12:58 +0900

    20230213

---
+++ debug/debug.py
@@ -0,0 +1,12 @@
+def find_dog(sound):
+    if sound=="멍멍":
+        return ("개가짖네")
+    
+    else:
+        return("다른동물이구나")
+    
+
+sound = "야옹"
+find_result=find_dog(sound)
+
+print(find_result)
