c2e1458baf8cbab591ebd0bee0c5f4072942ca75
hangilzzang <gkrwhddlwjqrl@gmail.com>
2023-02-14 18:00:09 +0900

    230214

---
+++ java_learning/while문과dowhile문.java
@@ -0,0 +1,13 @@
+package java_learning;
+
+public class while문과dowhile문 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+/* 
+ * do while문은 조건식에 의해 반복 실행한다는 점은 while문과 동일하지만 
+ * while은 조건식을 검사한뒤 블록 내부를 실행한다면 do while은 일단 먼저 블록 내부를 실행한뒤 반복할지를 결정한다는점에서 다르다
+ * 
+ */
