8f6de3485ee8128079740d395b0422b00a2ff699
hangilzzang <gkrwhddlwjqrl@gmail.com>
2023-02-27 15:50:00 +0900

    230227

---
+++ learning_java/src/hellohello.java
@@ -0,0 +1,5 @@
+public class hellohello {
+    public static void main(String[] args) {
+        System.out.println("이제된건가...?");
+    }
+}
--- learning_java/src/스위치와랜덤.java
+++ learning_java/src/스위치와랜덤.java
@@ -1,13 +1,11 @@
-package java_learning;
-
 public class 스위치와랜덤 {
     public static void main(String[] args) { // main 치면 단축키로서 바로 작성가능
-        int num = (int) (Math.random() * 6+1);
+        int num = (int) (Math.random() * 6 + 1);
         /*
          * math.random은 0보다 크고 1보자 작은수를 랜덤으로 출력함
          * int로 강제타입변환시 소수점을 잃게됨 따라서 해당코드는 동일한 확률로 1~6중의 수를 뽑는다고 할수있음
          */
-        switch(num){
+        switch (num) {
             case 1: // num이 1일경우
                 System.out.println("1번이 나왔습니다"); // 단축키 sysout
                 break;
@@ -18,13 +16,13 @@ public class 스위치와랜덤 {
                 System.out.println("3번이 나왔습니다");
                 break;
             case 4:
-                System.out.println("4번이 나왔습니다"); 
+                System.out.println("4번이 나왔습니다");
                 break;
             case 5:
                 System.out.println("5번이 나왔습니다");
                 break;
             default:
-                System.out.println("6번이 나왔습니다");               
+                System.out.println("6번이 나왔습니다");
         }
     }
 }
